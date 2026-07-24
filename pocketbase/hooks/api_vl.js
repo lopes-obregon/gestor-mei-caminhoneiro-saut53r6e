95a6d312e8669fbcd9599c315639ca6d2322bd1d
Renan <lopesobregon.renan5@gmail.com>
2026-07-24 18:16:55 -0300

    Auto-commit after full sync (build simulation)

--- pocketbase/hooks/api_vl.js
+++ pocketbase/hooks/api_vl.js
@@ -1,4 +1,4 @@
-export const apiVl = async () =>{
-      const baseUrl = $secrets.get('API_VL_LOCUCOES')
-      console.log('baseUrl', baseUrl);
+export const apiVl = async () => {
+  const baseUrl = $secrets.get('API_VL_LOCUCOES')
+  console.log('baseUrl', baseUrl)
 }
