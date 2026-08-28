cb48a63103e06916dbe141074e015c22a9d13330
Renan Obregon <lopesobregon.renan5@gmail.com>
2026-08-28 09:29:20 -0400

    Algumas config enviados e recebidos

--- pocketbase/hooks/set_verified_on_create.js
+++ pocketbase/hooks/set_verified_on_create.js
@@ -1,14 +1,8 @@
 // Todo novo usuário já nasce verificado e com payment_status = "paid".
 // O "mês grátis" passa a ser refletido diretamente como pagamento ativo,
 // dispensando a janela de 30 dias baseada em strftime.
-onRecordCreate((e) => {
-  const record = e.record
-  if (!record) {
-    return e.next()
-  }
-
-  record.set('verified', true)
-  record.set('payment_status', 'paid')
-
+onRecordCreateRequest((e) => {
+  e.record.set('verified', true)
+  e.record.set('payment_status', 'paid')
   return e.next()
 }, 'users')
