ba26a610ff36ed675877475a35cca67bf2471224
Renan <lopesobregon.renan5@gmail.com>
2026-08-31 20:31:00 -0300

    Auto-commit after full sync (build simulation)

--- pocketbase/hooks/sync_user_create.js
+++ pocketbase/hooks/sync_user_create.js
@@ -15,7 +15,7 @@ onRecordAfterCreateSuccess((e) => {
   let url = baseUrl
   if (url.endsWith('/')) url = url.slice(0, -1)
   url = url + '/users'
-  //console.log('url completa:' + url)
+  console.log('url completa:' + url)
   try {
     $http.send({
       url: url,
