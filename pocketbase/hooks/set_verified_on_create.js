6abd304a42e225aedcec72b45367081e78be74b8
Renan <lopesobregon.renan5@gmail.com>
2026-08-31 19:52:03 -0300

    Auto-commit after full sync (build simulation)

--- pocketbase/hooks/set_verified_on_create.js
+++ pocketbase/hooks/set_verified_on_create.js
@@ -6,5 +6,5 @@ onRecordCreate((e) => {
   e.record.setVerified(true)
   e.record.set('payment_status', 'paid')
   //$app.save(e.record)
-  e.record.next();// deixa o PocketBase persistir o registro normalmente
+  e.record.next() // deixa o PocketBase persistir o registro normalmente
 }, 'users')
